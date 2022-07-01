c00227f806e6c02e11d0cd60576d635a6dd26788
osewus <34625293+osewus@users.noreply.github.com>
2022-07-01 02:36:19 +0000

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def main():
-    l = dir(hidden_4)
-    for i in range(len(l)):
-        if(l[i][0] != '_'):
-            print("{}".format(l[i]))
-
 if __name__ == "__main__":
-    main()
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
