f82e61e3f214e9084bc748161b2951101c7f425f
osewus <34625293+osewus@users.noreply.github.com>
2022-07-19 01:58:42 +0000

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
- divides 2 integers and prints the result.
-    """
-
-
 def safe_print_division(a, b):
     try:
-        x = a / b
+        result = a / b
     except:
-        x = None
+        result = None
     finally:
-        print("Inside result: {}".format(x))
-        return x
+        print("Inside result: {}".format(result))
+        return (result)
