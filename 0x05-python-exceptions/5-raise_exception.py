e587364a179246e44d21635445ca624c80b2e517
osewus <34625293+osewus@users.noreply.github.com>
2022-07-19 01:42:33 +0000

    Update 5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""
-    function that raises a type exception
-    """
 
 
 def raise_exception():
-    try:
-        raise(TypeError)
-    except:
-        raise
+    raise TypeError
