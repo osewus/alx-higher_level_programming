c6508ca6c96e345f557ed51252150a1641116084
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-08-01 11:25:20 -0700

    inheritance

---
+++ 0x0A-python-inheritance/0-loookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Returns a list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Function that returns the list of available attributes and methods,
+    f an object
+    Args:
+        obj (class): object
+    Returns:
+        list: list of available attributes and methods of an object
+    """
+    return dir(obj)
