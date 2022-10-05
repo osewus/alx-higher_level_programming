fd3736bcd54cb20e0fac0d984ef515b5cd3336df
osewus <34625293+osewus@users.noreply.github.com>
2022-10-05 16:20:33 +0000

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-console.log(myVar.join('\n'));
+console.log('C is fun\nPython is cool\nJavascript is amazing');
