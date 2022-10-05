c1bd7e93f68d2493b81394100c35517067758285
osewus <34625293+osewus@users.noreply.github.com>
2022-10-05 16:56:20 +0000

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-const intRegex = /^[0-9]*?.[0-9]*$/;
-const a = process.argv[2];
-
-function factorial (a) {
-  if (a === 0) {
-    return 1;
-  }
-  return a * factorial(a - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-if (intRegex.test(a)) {
-  console.log(factorial(parseInt(a)));
-} else {
-  console.log(1);
-}
+console.log(factorial(Number(process.argv[2])));
