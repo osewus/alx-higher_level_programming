4dc53d56a2e9bc689bec81d9c786a3be676637d8
osewus <34625293+osewus@users.noreply.github.com>
2022-10-05 16:27:21 +0000

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const MyVar = process.argv[2];
-if (isNaN(MyVar) === true) {
-  console.log('Not a number');
+const intRegex = /^[0-9]*?.[0-9]*$/;
+
+if (intRegex.test(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
-  console.log('My number: ' + MyVar);
+  console.log('Not a number');
 }
