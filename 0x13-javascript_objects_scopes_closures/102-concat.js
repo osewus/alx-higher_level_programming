202925f3a08655e8a79f84b259156911b4802158
osewus <34625293+osewus@users.noreply.github.com>
2022-10-05 17:17:41 +0000

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-const nDict = {};
-for (const key in dict) {
-  if (nDict[dict[key]] === undefined) {
-    nDict[dict[key]] = [];
-  }
-  nDict[dict[key]].push(key);
-}
-console.log(nDict);
+const fs = require('fs');
+const A = process.argv[2];
+const B = process.argv[3];
+const C = process.argv[4];
+const tA = fs.readFileSync(A, 'utf8');
+const tB = fs.readFileSync(B, 'utf-8');
+fs.writeFileSync(C, tA + tB);
