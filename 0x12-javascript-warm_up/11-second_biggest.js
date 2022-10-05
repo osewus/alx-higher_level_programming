0d25f213911f28ce95cf94201a9529950c22a30c
osewus <34625293+osewus@users.noreply.github.com>
2022-10-05 16:45:06 +0000

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,9 @@
 #!/usr/bin/node
-function nextBiggest (arr) {
-  let max = 0; let result = 0;
-
-  for (const value of arr) {
-    const nr = Number(value);
-
-    if (nr > max) {
-      [result, max] = [max, nr];
-    } else if (nr < max && nr > result) {
-      result = nr;
-    }
-  }
-
-  return result;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-
-console.log(nextBiggest(process.argv));
